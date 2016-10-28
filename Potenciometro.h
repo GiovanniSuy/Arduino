3f641661b39c7921e37d6e9d5b53aeb5928c5392
GiovanniSuy <giovanni_ussuy97@hotmail.com>
2016-10-28 19:43:21 -0200

    Add files via upload

---
+++ Potenciometro.h
@@ -0,0 +1,37 @@
+#ifndef Potenciometro_H
+#define Potenciometro_H
+
+#if defined(ARDUINO) && ARDUINO < 100
+#include "WProgram.h"
+#else
+#include "Arduino.h"
+#endif
+
+const int MIN_VALUE = 0;
+cont int MAX_VALUE = 1024;
+
+class Potenciometro {
+    public:
+        Potenciometro(byte pino);
+        Potenciometro(byte pino, int setores);
+        Potenciometro(byte pino, String descricaoSetores[]);
+
+        int getValor();
+        int getValor(int minimo, int maximo);
+        //pot1.getValor(0,250);
+        //pot2.getValor();
+        //pot3.getValor(-100, 100);
+        //pot4.getValor(0,360);
+
+        int getSetor();
+        void setSetores(int setores);
+
+        String getDescricaoSetores();
+        void setDescricaoSetores(String descricaoSetores[]);
+    private:
+        byte pino;
+        int setores;
+        String *descricaoSetores;
+};
+
+#endif
